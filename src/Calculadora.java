582674c3b49edfdeca585a8196b6ddad998e7db9
Jeffrey <jefftorres021@gmail.com>
2024-08-13 18:08:31 -0500

    Agregado el método de suma

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -2,5 +2,11 @@ public class Calculadora {
     public static void main(String[] args) {
         System.out.println("Hola mundo!!!");
         System.out.println("Hola de nuevo!!!");
+
+        System.out.println(sumar(2, 4));
+    }
+
+    public static int sumar(int a, int b) {
+        return a + b;
     }
 }
