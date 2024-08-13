c39608da94d473c93927849c3cc769ec640f1f9d
Leonardo <leonardogalindo2612@gmail.com>
2024-08-13 18:19:22 -0500

    Agrego función restar

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -4,9 +4,14 @@ public class Calculadora {
         System.out.println("Hola de nuevo!!!");
 
         System.out.println(sumar(2, 4));
+        System.out.println(restar(4, 2));
     }
 
     public static int sumar(int a, int b) {
         return a + b;
+    }
+
+    public static int restar(int a, int b) {
+        return a - b;
     }
 }
